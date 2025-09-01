74441a885ee92202aab25ace14623fad399995b0
Rishav <iamrishav0127@gmail.com>
2025-09-01 20:01:45 +0530

    completed

--- todo_main/views.py
+++ todo_main/views.py
@@ -5,7 +5,9 @@ from todo_list.models import Task
 
 def home( request):
     tasks=Task.objects.filter(is_completed=False)
+    completed_tasks=Task.objects.filter(is_completed=True)
     context={
         'tasks':tasks,
+        'completed_tasks': completed_tasks
     }
     return render(request,'home.html', context)
